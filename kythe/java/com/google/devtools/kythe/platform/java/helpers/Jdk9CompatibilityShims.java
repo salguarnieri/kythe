199bd14cd0d9f96e8f14c143e0638ace927bead8
Sal <salguarnieri@google.com>
2023-05-05 14:46:38 -0700

    chore(java_indexer): match interface return type

--- kythe/java/com/google/devtools/kythe/platform/java/helpers/Jdk9CompatibilityShims.java
+++ kythe/java/com/google/devtools/kythe/platform/java/helpers/Jdk9CompatibilityShims.java
@@ -17,7 +17,6 @@
 package com.google.devtools.kythe.platform.java.helpers;
 
 import com.google.auto.service.AutoService;
-import com.google.common.collect.ImmutableList;
 import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.JCTree.JCCase;
 import com.sun.tools.javac.tree.JCTree.JCEnhancedForLoop;
@@ -38,7 +37,7 @@ public final class Jdk9CompatibilityShims implements JdkCompatibilityShims {
 
   /** Return the list of expressions from a JCCase object */
   @Override
-  public ImmutableList<JCExpression> getCaseExpressions(JCCase tree) {
+  public List<JCExpression> getCaseExpressions(JCCase tree) {
     JCExpression expr = tree.getExpression();
     if (expr == null) {
       return ImmutableList.of();
